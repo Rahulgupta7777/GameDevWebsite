80fb133977e7bcd0f8aa9678349a35f1baba0b70
Rahul Gupta <rahulgupta193246@gmail.com>
2026-02-07 02:07:48 +0530

    Remove deprecated `useNewUrlParser` and `useUnifiedTopology` options from Mongoose connection.

--- connection.js
+++ connection.js
@@ -7,10 +7,7 @@ const connect = async () => {
       return;
     }
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("Connected to MongoDB");
   } catch (error) {
